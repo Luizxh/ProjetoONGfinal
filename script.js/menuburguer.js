ca9010dc0bade07e467b56e33f9fbab8ea35dc25
luiz henrique-IA <luizhenriquefacudade09@gmail.com>
2025-10-26 10:34:35 -0300

    ATIVIDADE 2 | REALIZAÇÃO DA ESTILIZAÇÃO COM CSS3

--- script.js/menuburguer.js
+++
@@ -1,8 +0,0 @@
-
-  const botaoMenu = document.getElementById("btn-menu");
-  const menuLista = document.getElementById("menu-lista");
-
-  botaoMenu.addEventListener("click", () => {
-    menuLista.classList.toggle("mostrar");
-  });
-
